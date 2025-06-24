bea1ca3463188b273c1a9730f57de0309a00b8c3
Kishan Suhi <kishansuhir@gmail.com>
2025-06-24 12:36:40 -0400

    naem

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'AC IG Finder',
+  description: 'Created by FSgt Pradeesh and Sgt Suhirthan',
   generator: 'v0.dev',
 }
 
